6cfb35081b4aec7b2109cac7823c2ba837dce10a
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-09-16 11:00:08 -0600

    Adds instructions to create appliance list from file

--- src/methods/Methods.java
+++ src/methods/Methods.java
@@ -12,6 +12,13 @@ public class Methods {
 		public static List<Appliance> createApplianceList()
 		{
 			List<Appliance> allAppliances = new ArrayList<Appliance>();
+			// Read a line of the txt file
+			
+			// Parse it into a string array splitting at ;
+			
+			// Create and object using indexes of the string array
+			
+			// Append new appliance object to the allAppliances list
 			
 			return allAppliances;
 		};
