9ea477feddc8384896a0cee96f824b9575fa69eb
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-09-19 16:04:55 -0600

    Completes Project (Pre Testing)

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+import methods.Methods.ProgramMethods;
+
+import java.util.List;
+
+import appliances.*;
+
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// Calls display menu method to run program until user chooses to exit
+		ProgramMethods.displayMenu();	
+	}
+
+}
